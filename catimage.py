adfe22d793ba38eaeb6d4fbe39d03af716cbc18d
root <alarmcuddly@outlook.com>
2020-03-06 11:25:34 +0000

    replaced gooey with cli2gui

--- catimage.py
+++ catimage.py
@@ -7,17 +7,15 @@ Cat an image to the console. Inspired by img2txt
 https://github.com/hit9/img2txt.git by Chao Wang
 """
 
-from PIL import Image
 import platform
 import urllib.request
 import os
 import sys
 import ctypes
-from gooey import Gooey, GooeyParser
+import argparse
 
-# Gooey is great, but the user should opt in
-if "--gui" not in sys.argv:
-	sys.argv.append("--ignore-gooey")
+from PIL import Image
+from cli2gui import Cli2Gui
 
 def openImageToPx(imageName, maxLen, hd=False):
 	"""Get an array of pixels and the dimensions of these
@@ -97,7 +95,7 @@ def generateHDColour(imageName, maxLen, trueColour=True, char="\u2584"):
 	Returns:
 		str: string to print
 	"""
-	char = "\u2584" if char is None else char
+	char = "\u2584" if char in [None, ""] else char
 	pixels, width, height = openImageToPx(imageName, maxLen, hd=True)
 	result = "\x1b[2K\x1b[0m" # Clear line and reset
 	beforeFgColour = None
@@ -142,7 +140,7 @@ def generateColour(imageName, maxLen, trueColour=True, char="\u2588"):
 	Returns:
 		str: string to print
 	"""
-	char = "\u2588" if char is None else char
+	char = "\u2588" if char in [None, ""] else char
 	pixels, width, height = openImageToPx(imageName, maxLen)
 	result = "\x1b[2K\x1b[0m" # Clear line and reset
 	beforeFgColour = None
@@ -185,45 +183,13 @@ def generateGreyscale(imageName, maxLen):
 	return result
 
 
-def catImage(imageName, maxLen, colour, hd, trueColour, char):
-	if colour and hd:
-		result = generateHDColour(imageName, maxLen, trueColour, char)
-	elif colour and not hd:
-		result = generateColour(imageName, maxLen, trueColour, char)
-	else:
-		result = generateGreyscale(imageName, maxLen)
 
-	print(result)
+def handleArgs(args):
+	"""Handle arguments from the cli/ gui
 
-@Gooey(
-	program_name="CatImage", suppress_gooey_flag=True, default_size=(800, 600),
-	monospace_display=True, richtext_controls=True, image_dir="assets"
-)
-def main(): # pragma: no cover
-	parser = GooeyParser(description="Cat an image to the terminal")
-	parser.add_argument("image", type=str, metavar="Image",
-		help="Image file or url", widget="FileChooser")
-	parser.add_argument("--gui", action="store_true", metavar="GUI",
-		help="Start program as a GUI. Note: Has a dumb terminal")
-	parser.add_argument("-u", "--url", action="store_true", metavar="URL",
-		help="Image is a URL")
-	parser.add_argument("-b", "--big", action="store_true", metavar="Big",
-		help="Big image")
-	parser.add_argument("-c", "--char", action="store", metavar="Char",
-		help="Char to use in colour print use $'chr' for escaped chars")
-	parser.add_argument("-t", "--disable-truecolour", action="store_true", metavar="Disable Truecolor",
-		help="Disable output in truecolour")
-
-	group = parser.add_argument_group("Choose one of the following",
-		"Use the following arguments to change the look of the image")
-	mxg = group.add_mutually_exclusive_group()
-	mxg.add_argument("-g", "--greyscale", action="store_true", metavar="Greyscale",
-		help="Output image in greyscale (best for terminals that cannot handle ANSI)")
-	mxg.add_argument("-r", "--regular", action="store_true", metavar="Regular",
-		help="Output image in regular definition")
-
-	args = parser.parse_args()
-
+	Args:
+		args (argparse): arguments
+	"""
 	if args.url:
 		urllib.request.urlretrieve(args.image, "dowloadedImage.jpg")
 		args.image = "dowloadedImage.jpg"
@@ -233,8 +199,44 @@ def main(): # pragma: no cover
 		kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 	os.path.exists(args.image)
-	catImage(args.image, 130 if args.big else 78, not args.greyscale,
-		not args.regular, not args.disable_truecolour, args.char)
+
+	maxLen = 130 if args.big else 78
+	if not args.greyscale and not args.regular:
+		result = generateHDColour(args.image, maxLen, not args.disable_truecolour, args.char)
+	elif not args.greyscale and args.regular:
+		result = generateColour(args.image, maxLen, not args.disable_truecolour, args.char)
+	else:
+		result = generateGreyscale(args.image, maxLen)
+	print(result)
+
+@Cli2Gui(run_function=handleArgs, image="program_icon.png")
+def cli(): # pragma: no cover
+	parser = argparse.ArgumentParser(description="cat an image to the terminal")
+	parser.add_argument("image", type=str,
+		help="image file or url")
+	parser.add_argument("-u", "--url", action="store_true",
+		help="image is a URL")
+	parser.add_argument("-b", "--big", action="store_true",
+		help="big image")
+	parser.add_argument("-c", "--char", action="store",
+		help="char to use in colour print use $'chr' for escaped chars")
+	parser.add_argument("-t", "--disable-truecolour", action="store_true",
+		help="disable output in truecolour")
+
+	group = parser.add_argument_group("choose one of the following",
+		"use the following arguments to change the look of the image")
+	mxg = group.add_mutually_exclusive_group()
+	mxg.add_argument("-g", "--greyscale", action="store_true",
+		help="output image in greyscale (best for terminals that cannot handle ANSI)")
+	mxg.add_argument("-r", "--regular", action="store_true",
+		help="output image in regular definition")
+
+	args = parser.parse_args()
+
+	handleArgs(args)
+
+
+
 
 if __name__ == "__main__": # pragma: no cover
-	main()
+	cli()
